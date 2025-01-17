56c30aafcadfb681c51140933bfb243c8147a62b
s3xmaxman <badside3919@gmail.com>
2025-01-17 19:56:24 +0900

    Enhance ESLint configuration and update Home component layout
    - Added Tailwind CSS plugin to ESLint configuration for improved linting support.
    - Disabled specific TypeScript rules to streamline development.
    - Replaced the default Home component layout with a simplified "Hello world!" message.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,19 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "plugin:tailwindcss/recommended"
+  ),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-non-null-asserted-optional-chain": "off",
+      "@typescript-eslint/no-unused-expressions": "off",
+    },
+  },
 ];
 
 export default eslintConfig;

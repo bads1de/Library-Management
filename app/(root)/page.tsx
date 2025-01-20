5df872fcc9b1348fb8d8892036146a953580f7fd
s3xmaxman <badside3919@gmail.com>
2025-01-21 07:45:29 +0900

    Enhance Home page and BookOverview component
    - Updated Home component to display the first book's details using the BookOverview component.
    - Modified BookList to show a list of sample books with a title and custom container class.
    - Refactored BookOverview component to accept props for title, author, genre, rating, total copies, available copies, description, cover color, and cover URL, providing a more detailed book overview.
    - Added a borrow button with an associated book cover image for improved user interaction.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,18 @@
 import Image from "next/image";
 import BookOverview from "@/components/BookOverview";
 import BookList from "@/components/BookList";
+import { sampleBooks } from "@/constants";
 
 export default function Home() {
   return (
     <div>
-      <BookOverview />
-      <BookList />
+      <BookOverview {...sampleBooks[0]} />
+
+      <BookList
+        title="Latest Books"
+        books={sampleBooks}
+        containerClassName="mt-28"
+      />
     </div>
   );
 }

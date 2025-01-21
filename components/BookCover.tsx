26ccf908ada72a0fed2f3fb503c27e309348be12
s3xmaxman <badside3919@gmail.com>
2025-01-21 20:11:20 +0900

    Enhance Next.js configuration and BookCover component
    - Updated next.config.ts to include remote image patterns for improved image handling.
    - Configured TypeScript and ESLint settings to ignore build errors during development.
    - Modified BookCover component to utilize Next.js Image component for optimized image rendering.
    - Added a new div for displaying the book cover image with responsive styling.

--- components/BookCover.tsx
+++ components/BookCover.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { cn } from "@/lib/utils";
 import BookCoverSvg from "./BookCoverSvg";
+import Image from "next/image";
 
 type BookCoverVariant = "extraSmall" | "small" | "medium" | "regular" | "wide";
 
@@ -34,6 +35,18 @@ const BookCover = ({
       )}
     >
       <BookCoverSvg coverColor={coverColor} />
+
+      <div
+        className="absolute z-10"
+        style={{ left: "12%", width: "87.5%", height: "88%" }}
+      >
+        <Image
+          src={coverImage}
+          alt="book cover"
+          fill
+          className="object-fill rounded-sm"
+        />
+      </div>
     </div>
   );
 };

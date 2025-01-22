19089691d153cee66530e0c60ea27c612d346cf7
s3xmaxman <badside3919@gmail.com>
2025-01-22 16:09:36 +0900

    Enhance Auth Layout Component with Improved Structure and Visuals
    - Refactored Layout component to accept children as props for better flexibility.
    - Added a main container with sections for the authentication form and illustration.
    - Integrated Next.js Image component for optimized logo and illustration rendering.
    - Improved layout styling for a more engaging user experience in the authentication flow.

---
+++ components/AuthForm.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  DefaultValues,
+  FieldValues,
+  Path,
+  SubmitHandler,
+  useForm,
+  UseFormReturn,
+} from "react-hook-form";
+import { ZodType } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import Link from "next/link";
+import { FIELD_NAMES, FIELD_TYPES } from "@/constants";
+import { toast } from "@/hooks/use-toast";
+import { useRouter } from "next/navigation";
+
+interface Props<T extends FieldValues> {
+  schema: ZodType<T>;
+  defaultValues: T;
+  onSubmit: (data: T) => Promise<{ success: boolean; error?: string }>;
+  type: "SIGN_IN" | "SIGN_UP";
+}
+
+const AuthForm = <T extends FieldValues>({
+  schema,
+  defaultValues,
+  onSubmit,
+  type,
+}: Props<T>) => {
+  return (
+    <div>
+      <h1>Auth Form</h1>
+    </div>
+  );
+};
+
+export default AuthForm;

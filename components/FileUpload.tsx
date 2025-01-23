af736e9ac8f75dfa77c0856a04479a8b66baa977
s3xmaxman <badside3919@gmail.com>
2025-01-23 14:30:47 +0900

    Add ImageKit integration for file uploads in FileUpload component

--- components/FileUpload.tsx
+++ components/FileUpload.tsx
@@ -1,4 +1,45 @@
-import React from "react";
+"use client";
+
+import { IKImage, ImageKitProvider, IKUpload, IKVideo } from "imagekitio-next";
+import config from "@/lib/config";
+import ImageKit from "imagekit";
+import { useRef, useState } from "react";
+import Image from "next/image";
+import { toast } from "@/hooks/use-toast";
+import { cn } from "@/lib/utils";
+
+const {
+  env: {
+    imagekit: { publicKey, urlEndpoint },
+  },
+} = config;
+
+const auth = async () => {
+  try {
+    const response = await fetch(`${config.env.apiEndpoint}/api/auth/imagekit`);
+
+    if (!response.ok) {
+      const errorText = await response.text();
+
+      throw new Error(
+        `Request failed with status ${response.status}: ${errorText}`
+      );
+    }
+
+    const data = await response.json();
+
+    const { signature, expire, token } = data;
+
+    return {
+      signature,
+      expire,
+      token,
+    };
+  } catch (error: any) {
+    console.error("Error during authentication:", error);
+    throw new Error("Failed to authenticate with ImageKit.");
+  }
+};
 
 const FileUpload = () => {
   return <div>FileUpload</div>;

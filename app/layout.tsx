6289b8dfaf9bd61efb9eba4568b0efd3f97300c0
s3xmaxman <badside3919@gmail.com>
2025-01-24 13:16:28 +0900

    Add SessionProvider to RootLayout for authentication context
    - Wrapped RootLayout with SessionProvider to enable authentication state management
    - Made RootLayout an async function to fetch session data
    - Integrated auth() to retrieve session information

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import localFont from "next/font/local";
+import { auth } from "@/auth";
+import { SessionProvider } from "next-auth/react";
 import { Toaster } from "sonner";
 
 const ibmPlexSans = localFont({
@@ -25,19 +27,22 @@ export const metadata: Metadata = {
     "BookWise is a book borrowing university library management solution.",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await auth();
   return (
     <html lang="en">
-      <body
-        className={`${ibmPlexSans.className} ${bebasNeue.variable} antialiased`}
-      >
-        {children}
-      </body>
-      <Toaster />
+      <SessionProvider session={session}>
+        <body
+          className={`${ibmPlexSans.className} ${bebasNeue.variable} antialiased`}
+        >
+          {children}
+        </body>
+        <Toaster />
+      </SessionProvider>
     </html>
   );
 }

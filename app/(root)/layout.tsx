1bcb656c5eb9b58f2a5a67e00bad6e31a308472e
s3xmaxman <badside3919@gmail.com>
2025-01-27 02:51:07 +0900

    feat: Add last activity tracking for authenticated users
    Implements a mechanism to update the user's last activity date in the root layout when an authenticated session is detected. This change ensures that user engagement can be tracked without interrupting the main application flow.

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,42 @@
+import { auth } from "@/auth";
 import Header from "@/components/Header";
+import { db } from "@/database/drizzle";
+import { users } from "@/database/schema";
+import { eq } from "drizzle-orm";
+import { redirect } from "next/navigation";
+import { after } from "next/server";
 import React, { ReactNode } from "react";
 
-const Layout = ({ children }: { children: ReactNode }) => {
+const Layout = async ({ children }: { children: ReactNode }) => {
+  const session = await auth();
+
+  if (!session) {
+    return redirect("/sign-in");
+  }
+
+  // ユーザーの最終アクティビティ日を更新する処理
+  after(async () => {
+    // セッションにユーザーIDが存在しない場合は処理を終了
+    if (!session?.user?.id) return;
+
+    // データベースから現在のユーザー情報を取得
+    const user = await db
+      .select()
+      .from(users)
+      .where(eq(users.id, session?.user?.id))
+      .limit(1);
+
+    // 今日の日付が既に最終アクティビティ日として記録されている場合は更新不要
+    if (user[0].lastActivityDate === new Date().toISOString().slice(0, 10))
+      return;
+
+    // 最終アクティビティ日を今日の日付に更新
+    await db
+      .update(users)
+      .set({ lastActivityDate: new Date().toISOString().slice(0, 10) })
+      .where(eq(users.id, session?.user?.id));
+  });
+
   return (
     <main className="root-container">
       <div className="mx-auto max-w-7xl">

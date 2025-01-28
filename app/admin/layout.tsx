937e8d46ee9aeafad9a1ae57fcbfb6db8a32f0bb
s3xmaxman <badside3919@gmail.com>
2025-01-28 20:40:23 +0900

    feat: Add Sidebar and Header to admin layout
    Enhances the admin layout by:
    - Importing and integrating Sidebar and Header components
    - Passing session data to both components
    - Improving the overall structure of the admin layout

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -2,6 +2,8 @@ import React, { ReactNode } from "react";
 import { auth } from "@/auth";
 import { redirect } from "next/navigation";
 import "@/styles/admin.css";
+import Sidebar from "@/components/admin/Sidebar";
+import Header from "@/components/admin/Header";
 
 const layout = async ({ children }: { children: ReactNode }) => {
   const session = await auth();
@@ -12,7 +14,11 @@ const layout = async ({ children }: { children: ReactNode }) => {
 
   return (
     <main className="flex min-h-screen w-full flex-row">
-      <div className="admin-container">{children}</div>
+      <Sidebar session={session} />
+      <div className="admin-container">
+        <Header session={session} />
+        {children}
+      </div>
     </main>
   );
 };
